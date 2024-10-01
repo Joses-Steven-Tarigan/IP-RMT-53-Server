789d92f404fa8d2107aa0e9b36f0e055d4c002dd
Joses-Steven-Tarigan <steventarigan82@gmail.com>
2024-10-01 22:59:54 +0700

    Feat: -add route user login and jwt

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,6 @@
 const {User}= require('../models');
+const { generateToken } = require('../helpers/jwt');
+const { compare } = require('../helpers/bcrypt');
 
 
 
@@ -7,6 +9,31 @@ exports.register = async (req, res, next) => {
     try {
         await User.create({ username, email, password })
         res.status(201).json({ message: 'User has been registered' })
+    } catch (error) {
+        next(error)
+    }
+}
+
+exports.login = async (req, res, next) => {
+    let { email, password } = req.body
+    try {
+        if (!email || !password) throw { name: "EmailPasswordNotFound" }
+
+        let user = await User.findOne({
+            where: {
+                email
+            }
+        });
+        if (!user) throw { name: "InvalidEmailPassword" }
+
+        if (!compare(password, user.password)) {
+            throw { name: "InvalidEmailPassword" }
+        }
+
+        const access_token = generateToken(user);
+
+        res.status(200).json({ access_token })
+
     } catch (error) {
         next(error)
     }
