bb3ee99f4757193cffb02648eeed4f3f7b98f07b
Joses-Steven-Tarigan <steventarigan82@gmail.com>
2024-10-02 16:49:33 +0700

    Feat: add router for user

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,12 +1,15 @@
 
 
 const router = require('express').Router();
-const { register } = require('../controllers/userController');
+const { addService, getService } = require('../controllers/serviceController');
+const { register, login } = require('../controllers/userController');
 const errorHandler=require('../middlewares/errorHandler');
 
 
 router.post("/register", register)
-router.post("/login")
+router.post("/login", login)
+router.post("/service", addService)
+router.get("/service", getService)
 
 
 
